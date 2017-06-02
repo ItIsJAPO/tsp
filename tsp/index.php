597d332958de80be00e6624e60ecf7604052a4cf
Jose Antonio Pino <ItIsJAPO@gmail.com>
2017-06-01 21:52:14 -0500

    Se arreglaron los errores.
    Trabaja segun lo pedido a Melchor Lopez

--- tsp/index.php
+++ tsp/index.php
@@ -2,15 +2,13 @@
 <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no"/>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title>Using MySQL and PHP with Google Maps</title>
+    <title>PHP with Google Maps</title>
     <style>
-        /* Always set the map height explicitly to define the size of the div
-         * element that contains the map. */
+
         #map {
             height: 100%;
         }
 
-        /* Optional: Makes the sample page fill the window. */
         html, body {
             height: 100%;
             margin: 0;
@@ -23,47 +21,37 @@
 <div id="map"></div>
 
 <script>
-    var customLabel = {
-        restaurant: {
-            label: 'R'
-        },
-        bar: {
-            label: 'B'
-        }
-    };
-
     function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-            center: new google.maps.LatLng(21.74367551,-90.45217084),
+            center: new google.maps.LatLng(21.74367551, -90.45217084),
             zoom: 8
         });
         var infoWindow = new google.maps.InfoWindow;
 
-        // Change this depending on the name of your PHP or XML file
         downloadUrl('test.xml', function (data) {
             var xml = data.responseXML;
             var markers = xml.documentElement.getElementsByTagName('marker');
             Array.prototype.forEach.call(markers, function (markerElem) {
-               var hour = markerElem.getAttribute('hour');
-                var kkk = markerElem.getAttribute('id');
+                var name = markerElem.getAttribute('name');
+                var hour = markerElem.getAttribute('hour');
+
                 var point = new google.maps.LatLng(
                     parseFloat(markerElem.getAttribute('lat')),
                     parseFloat(markerElem.getAttribute('lng')));
 
                 var infowincontent = document.createElement('div');
                 var strong = document.createElement('strong');
-                strong.textContent = 'temp'
-                infowincontent.appendChild(strong);
+                strong.textContent = name,
+                    infowincontent.appendChild(strong);
                 infowincontent.appendChild(document.createElement('br'));
 
                 var text = document.createElement('text');
-                text.textContent = hour
+                text.textContent = hour + ' hrs'
                 infowincontent.appendChild(text);
-//                var icon = customLabel[type] || {};
                 var marker = new google.maps.Marker({
                     map: map,
                     position: point,
-                    label: ''
+                    label: name
                 });
                 marker.addListener('click', function () {
                     infoWindow.setContent(infowincontent);
@@ -78,7 +66,6 @@
         var request = window.ActiveXObject ?
             new ActiveXObject('Microsoft.XMLHTTP') :
             new XMLHttpRequest;
-
         request.onreadystatechange = function () {
             if (request.readyState == 4) {
                 request.onreadystatechange = doNothing;
